24f93bd6cc13c09b6917229be86bca506d0dc63c
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-19 12:40:22 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,32 @@
 //Setting date to countdown to
-let countDownDate = new Date("Jan 01,2022 00:00:00").getTime();
+const countDownDate = new Date("Jan 01, 2022 00:00:00").getTime();
 
-//Countdown by seconds to mins to hours to days
+    //Countdown by seconds to mins to hours to days
     let myfunc = setInterval(function() {
         let now = new Date().getTime();
         let timeleft = countDownDate - now;
 
-    // Setting days, hours, mins and secs to go 
-    // 1000 milliseconds, 60 secs, 60 mins, 24 hours 
-        let days = Math.floor(timeleft /(1000 * 60 * 60 * 24));
-        let hours = Math.floor((timeleft %(1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeleft % (1000 *60 *60)) / (1000 * 60))
+        // Setting days, hours, mins and secs to go 
+        // 1000 milliseconds, 60 secs, 60 mins, 24 hours 
+        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-
-    //Displaying days, hours, mins and secs
-        document.getElementById("days").innerHTML = days + "d"
-        document.getElementById("hours").innerHTML = hours + "h"
-        document.getElementById("mins").innerHTML = minutes + "m"
-        document.getElementById("secons").innerHTML + seconds + "s"
-
-    //Displaying countdown ending message
+    
+        //Displaying days, hours, mins and secs
+        document.getElementById("days").innerHTML = days + "d "        
+        document.getElementById("hours").innerHTML = hours + "h " 
+        document.getElementById("mins").innerHTML = minutes + "m " 
+        document.getElementById("secs").innerHTML = seconds + "s " 
+    
+        //Displaying countdown ending message
         if (timeleft < 0) {
             clearInterval(myfunc);
             document.getElementById("days").innerHTML = ""
-            document.getElementById("hours").innerHTML = ""
+            document.getElementById("hours").innerHTML = "" 
             document.getElementById("mins").innerHTML = ""
             document.getElementById("secs").innerHTML = ""
-            document.getElementById("end").innerHTML = "!!!HAPPY NEW YEAR!!!"
+            document.getElementById("end").innerHTML = "TIME UP!!";
         }
-    },
-
+    }, 
     1000);
