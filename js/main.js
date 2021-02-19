1255d90646944b570469201e0489709c33ca4fc4
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-19 09:34:41 -0500

    js/main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 //Setting date to countdown to
 let countDownDate = new Date("Jan 01,2022 00:00:00").getTime();
 
-//countdown seconds
+//countdown by seconds to mins to hours to days
     let myfunc = setInterval(function() {
         let now = new Date().getTime();
         let timeleft = countDownDate - now;
 
+    // setting days, hours, mins and secs to go 
+    // 1000 milliseconds, 60 secs, 60 mins, 24 hours 
+        let days = Math.floor(timeleft /(1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeleft %(1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeleft % (1000 *60 *60)) / 1000 * 60))
+        let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
-    }
