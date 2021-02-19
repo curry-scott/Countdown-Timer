62e30a5de00e7afe5621816a28d3b530733d0d07
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-19 09:07:32 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Setting date to countdown to
+let countDownDate = new Date("Jan 01,2022 00:00:00").getTime();
+
+//countdown seconds
+    let myfunc = setInterval(function() {
+        let now = new Date().getTime();
+        let timeleft = countDownDate - now;
+
+        
+    }
