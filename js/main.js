99652febb1b09d522e61980b0ee3ae221caa51ef
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-21 22:52:54 -0500

    update

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const countDownDate = new Date("Jan 01, 2022 00:00:00").getTime();
 
         // Setting days, hours, mins and secs to go 
         // 1000 milliseconds, 60 secs, 60 mins, 24 hours 
-        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+        let days = Math.floor(timeleft / (1000 * 60 * 60 * 24)); 
         let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
